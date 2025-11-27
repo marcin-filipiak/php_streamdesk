0cff068b0cfc3d09f490dbf082710ebe3cb5199d
marcin-filipiak <m.filipiak@noweenergie.org>
2025-11-27 15:48:35 +0100

    ui

--- view/dashboard.php
+++ view/dashboard.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <title><?= htmlspecialchars(SITE_TITLE) ?></title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="<?= BASE_URL ?>/assets/css/styles.css" rel="stylesheet">
+<link href="<?= BASE_URL ?>/assets/css/styles.css?v=<?= date('YmdHi') ?>" rel="stylesheet">
 </head>
 <body class="bg-light">
 
